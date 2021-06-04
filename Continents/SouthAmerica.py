b3ba4d19b8eef62df46619d3112aadb5c4a48476
Sookeyy-12 <82956207+Sookeyy-12@users.noreply.github.com>
2021-06-04 22:27:16 +0530

    fixed classes

--- Continents/SouthAmerica.py
+++ Continents/SouthAmerica.py
@@ -1,60 +1,60 @@
 import requests
 import json
 
-class SouthAmerica():
-    def __init__(self):
-        self.url = 'https://corona.lmao.ninja/v2/continents/south%20america?yesterday&strict'
+url = 'https://corona.lmao.ninja/v2/continents/south%20america?yesterday&strict'
 
-    def get_cases(self):
-        response = requests.get(self.url)
+class SouthAmerica():
+
+    def get_cases():
+        response = requests.get(url)
         json_data = json.loads(response.text)
         cases = json_data['cases']
-        return cases
+        return str(cases)
 
-    def get_todaycases(self):
-        response = requests.get(self.url)
+    def get_todaycases():
+        response = requests.get(url)
         json_data = json.loads(response.text)
         today_cases = json_data['todayCases']
-        return today_cases
+        return str(today_cases)
 
-    def get_deaths(self):
-        response = requests.get(self.url)
+    def get_deaths():
+        response = requests.get(url)
         json_data = json.loads(response.text)
         deaths = json_data['deaths']
-        return deaths
+        return str(deaths)
 
-    def get_todaydeaths(self):
-        response = requests.get(self.url)
+    def get_todaydeaths():
+        response = requests.get(url)
         json_data = json.loads(response.text)
         today_deaths = json_data['todayDeaths']
-        return today_deaths
+        return str(today_deaths)
 
-    def get_recovered(self):
-        response = requests.get(self.url)
+    def get_recovered():
+        response = requests.get(url)
         json_data = json.loads(response.text)
         recovered = json_data['recovered']
-        return recovered
+        return str(recovered)
 
-    def get_todayrecovered(self):
-        response = requests.get(self.url)
+    def get_todayrecovered():
+        response = requests.get(url)
         json_data = json.loads(response.text)
         today_recovered = json_data['todayRecovered']
-        return today_recovered
+        return str(today_recovered)
 
-    def get_active(self):
-        response = requests.get(self.url)
+    def get_active():
+        response = requests.get(url)
         json_data = json.loads(response.text)
         active = json_data['active']
-        return active
+        return str(active)
 
-    def get_population(self):
-        response = requests.get(self.url)
+    def get_population():
+        response = requests.get(url)
         json_data = json.loads(response.text)
         population = json_data['population']
-        return population
+        return str(population)
 
-    def get_tests(self):
-        response = requests.get(self.url)
+    def get_tests():
+        response = requests.get(url)
         json_data = json.loads(response.text)
         tests = json_data['tests']
-        return tests
+        return str(tests)
