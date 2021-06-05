1b739f566778883570ecd05a51eb6ef1b6502f93
Sookeyy-12 <82956207+Sookeyy-12@users.noreply.github.com>
2021-06-05 14:38:24 +0530

    added country route

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from api.Countries import Countries
 import os
 from flask import Flask, render_template
 from dotenv import load_dotenv
@@ -7,6 +8,7 @@ app = Flask(__name__)
 
 from api.Continents import Continents
 continents = Continents()
+countries = Countries()
 
 @app.route("/")
 def home():
@@ -15,6 +17,10 @@ def home():
 @app.route("/continents/<name>")
 def return_data(name):
     return continents.get_data(name)
+
+@app.route("/countries/<name>")
+def return_data(name):
+    return countries.get_data(name)
 
 if __name__ == "__main__":
     app.run(port=os.getenv("PORT"))
